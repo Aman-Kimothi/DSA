72ae06b2260dced85b44035ce2a9354b0d1034a4
Aman Kimothi <kimothiaman@gmail.com>
2017-06-19 10:17:00 +0530

    Comb Sort

--- Sorting/Comb_Sort.java
+++ Sorting/Comb_Sort.java
@@ -3,25 +3,25 @@ class Comb_sort
 	static void comb_sort(int arr[] , boolean isAscending)  {
 	
 		int len = arr.length;
-		boolean flag = true;				// Using a pass flag increases the efficiency. 
+		boolean flag = true;			// Using a pass flag increases the efficiency. 
 		
-		// It's like Bubble sort but it has different factor for looping i.e  length/1.3
+		// It's like Bubble sort but it has different factor for looping  i.e length/1.3
 		
 		for(int i=(int) (len/1.3) ; flag || i > 0 ; i /= 1.3)  {
 			
 			flag = false;				//We use a flag to keep a check on whether there are no swaps in a whole pass
 		
-		for(int j = 0 ; (i + j) < len ; j++) {
+			for(int j = 0 ; (i + j) < len ; j++) {
 				
 				if(isAscending && arr[j] > arr[i + j]) { 	  	// Ascending Order 		     
-					int temp = arr[j];							// swap the values
+					int temp = arr[j];				// swap the values
 					arr[j] = arr[i + j];
 					arr[i + j] = temp;
 					flag = true;
 				}
 				
 				if(!isAscending && arr[j] < arr[i + j]) { 	  	// Descending Order
-					int temp = arr[j];							// swap the values
+					int temp = arr[j];				// swap the values
 					arr[j] = arr[i + j];
 					arr[i + j] = temp;
 					flag = true;
