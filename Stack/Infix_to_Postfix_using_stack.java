f8cd85989eea196ab8dedeb5b15d9ba872592201
Aman-Kimothi <kimothiaman@gmail.com>
2017-06-29 13:07:38 +0530

    Infix to Postfix using stack

---
+++ Stack/Infix_to_Postfix_using_stack.java
@@ -0,0 +1,122 @@
+/*
+	Conversion of infix into postfix using stack and a class 'Operators' for prioritising the precedence of the operators.
+	
+	Infix Expression 	: 	a+b*(c^d-e)^(f+g*h)-i 
+	Postfix Expression 	: 	abcd^e-*fgh*+^+i-
+	
+	Author : Aman Kimothi
+	E-mail : kimothiaman@gmail.com
+	Github : https://github.com/Aman-Kimothi/DSA
+*/
+
+
+import java.util.Stack;
+
+class Operators {
+		
+		/* This is a class where you can add as many operators as you want with their priority(precedence) number.
+		 This is very efficient as you can add as many operators as you want.  
+		 
+		 2D-Array is used here for checking the precedence of the operator. 
+		 HashMap can also be used instead of array which is easier to implement as hashmap.get(key) is mostly O(1).	*/
+		
+		char arr[][] ;		// array which stores the operators with their priority (precedence)
+		
+		public Operators() {
+			arr = new char[][] {   {'+' , '-' ,'*' ,'/' ,'^'} ,
+								   {'1' ,'1' ,'2' ,'2' ,'3' }
+							   };
+		}
+		
+		/* This function is to check whether the precedence of the stack character is greater than or equal 
+			to the precendence of the ch character	*/
+			
+		boolean getPriority(char ch , char stack) {
+			
+			int cur = 0 , stack_ = 0;			// to store the precedence number of the parameter chars
+			
+			for(int i = 0; i < arr[0].length; i++) {
+				if(arr[0][i] == ch)
+					cur = arr[1][i];
+				if(arr[0][i] == stack)
+					stack_ = arr[1][i];	
+			}
+			
+			if(cur <= stack_)					// if the precedence is greater than or equal to.
+				return true;
+			else
+				return false;
+		}
+}
+
+class Infix_to_Postfix_using_stack {
+	
+	String infix_to_postfix(String st) {
+	
+		Stack<Character> stack = new Stack<Character>();
+		String result = "";
+		int len = st.length();
+		Operators priority = new Operators();
+		
+		for(int i = 0; i < len; i++) {
+			char ch = st.charAt(i);
+			
+			if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))		// if it is an operand
+				result += ch;
+			else {
+				
+				if(ch == '(') {				// push the '(' into the stack
+					stack.push(ch);
+					continue;
+				}
+				else if(ch == ')') {		// pop all the elements until you find '(' in the stack
+				
+ 					while (!stack.isEmpty() && stack.peek() != '(')
+                    	result += stack.pop();
+						
+					if (!stack.isEmpty() && stack.peek() != '(')		// invalid expression 
+						return "Invalid Expression"; 		
+					
+                    stack.pop();	
+				}
+				else {			// when there is a operator
+						
+					while(true)  {
+						
+						if(stack.isEmpty() || stack.peek() == '(')
+							break;
+				
+						if(priority.getPriority(ch , stack.peek()))		// getting the precedence of the operators 
+							result += stack.pop();
+						else
+							break;
+					}
+					stack.push(ch);		// pushing the current element in the stack
+				}
+			}	
+		}
+		while(!stack.isEmpty()) 		// popping all the operators left in the stack
+			result += stack.pop();		
+		
+		return result;
+	}
+	
+	public static void main(String args[]) {
+		
+		Infix_to_Postfix_using_stack obj = new Infix_to_Postfix_using_stack();
+		
+		String infix = "(A+B)*C-(D-E)*(F+G)";
+		
+		String postfix = obj.infix_to_postfix(infix);
+		
+		System.out.println("Infix Exp : " +infix);
+		System.out.println("Postfix Exp : " +postfix);
+		
+		infix = "a+b*(c^d-e)^(f+g*h)-i";
+		
+		postfix = obj.infix_to_postfix(infix);
+		
+		System.out.println("\nInfix Exp : " +infix);
+		System.out.println("Postfix Exp : " +postfix);
+	}
+}
---
+++ Stack/Stack_using_linked_list.java
@@ -0,0 +1,113 @@
+/*
+	Implementation of a stack using a linked list with functions.
+	
+	push(), pop(), isEmpty(), peek() and print()
+
+	Author : Aman Kimothi
+	E-mail : kimothiaman@gmail.com
+	Github : https://github.com/Aman-Kimothi/DSA
+*/
+
+
+class Stack_using_linked_list {
+	
+	Node head;
+	
+	static class Node {
+		int data;
+		Node next;
+		public Node(int d) {
+			data = d;
+			next = null;
+		}
+	}
+	
+	public Stack_using_linked_list() {
+		head = null;
+	}
+	
+	void push(int data) {
+		Node new_node = new Node(data);
+		new_node.next = head;				// Linking the head to the next of the new node
+		head = new_node;					// Making the new node as the head
+	}
+	
+	int pop() {
+		if(head == null) {					// Underflow condition
+			System.out.println("Stack Underflow");
+			return Integer.MIN_VALUE;
+		}
+		Node temp = head;					// Storing the head in a node
+		head = head.next;					// Making the next node as the new node i.e removing the head node
+		return temp.data;
+	}
+	
+	boolean isEmpty() {
+		if(head == null)
+			return true;
+		return false;	
+	}
+	
+	int peek() {					// Returning the head element
+		if(head == null)
+			return Integer.MIN_VALUE;
+		return head.data;
+	}
+	
+	void print() {					// Printing all the elements stored in the stack
+		
+		if(head == null) {
+			System.out.println("Stack is empty");
+			return ;
+		}
+		
+		Node temp = head;
+		System.out.print("Stack elements are : ");
+		
+		while(temp != null) {
+			System.out.print(temp.data + "\t");
+			temp = temp.next;
+		}
+		System.out.println();	
+	}
+	
+	public static void main(String args[]) {
+		
+		Stack_using_linked_list st = new Stack_using_linked_list();
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+		st.push(5);
+		st.push(6);
+		
+		int pop;
+		
+		if((pop = st.pop()) != Integer.MIN_VALUE)
+			System.out.println("Popped value : " + pop);
+			
+		if((pop = st.pop()) != Integer.MIN_VALUE)
+			System.out.println("Popped value : " + pop);
+		
+		if((pop = st.pop()) != Integer.MIN_VALUE)
+			System.out.println("Popped value : " + pop);
+			
+		if((pop = st.pop()) != Integer.MIN_VALUE)
+			System.out.println("Popped value : " + pop);
+			
+
+		st.push(7);
+		if(st.isEmpty())
+			System.out.println("The stack is empty");
+		else	
+			System.out.println("The stack is not empty");
+		
+	
+		st.push(8);
+		if((pop = st.peek()) != Integer.MIN_VALUE)
+			System.out.println("Peeked value : " + pop);
+		
+		st.push(9);
+		st.print();
+	}
+}
